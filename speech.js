ff339f9b60a4c4593f5fc8d7ef452b53d284f445
tekyto <72567209+tekyto@users.noreply.github.com>
2021-01-19 21:31:59 +0200

    Update speech.js

--- speech.js
+++ speech.js
@@ -21,4 +21,4 @@ recognition.onresult =on_results;
 function on_results(e)
 {
 	document.getElementById("text").innerHTML +=  "Ati  rostit cuvantul:   "+   e.results[0][0].transcript   +   ",   acuratete: "+e.results[0][0].confidence + "<br>"
-]	
+}	
